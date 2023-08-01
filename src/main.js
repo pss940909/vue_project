1010661e92d9a9c8f58d4d52125d8fdcbb133f69
pss940909 <pss940909@gmail.com>
2023-08-01 15:38:35 +0800

    登入功能 & vue axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+// vue axios
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-createApp(App).use(router).mount("#app");
+// ----------------------------------
+const app = createApp(App);
+app.use(VueAxios, axios);
+app.use(router);
+app.mount("#app");
